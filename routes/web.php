5a3268943db1b52cd01b83c63fd685bf14caaff9
Rango <edsk8martinez97@gmail.com>
2020-03-03 12:57:30 -0600

    Firebase

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('firebase','FirebaseController@index');
+
+Route::get('firebase-get-data', 'FirebaseController@getData');
